d850135328fa223edbe38004c8efa6cda5bd3bef
saiTeja <nedunoorisaiteja2108@gmail.com>
2025-08-04 18:36:50 +0530

    Initial commit

---
+++ src/main/java/com/fitnotesmicroservices/backend/model/Workout.java
@@ -0,0 +1,28 @@
+package com.fitnotesmicroservices.backend.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity // entity means model 
+@Data // Lombok annotation to generate getters and setters 
+@Table(name="workout")
+public class Workout 
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    private int set;
+    private int reps;
+    private int supportreps;
+    private int halfreps;
+    private double weight;
+    private String notes;
+}
